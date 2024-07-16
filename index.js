c12c810d14550380fb61f7d3fc63fcea525dc937
indranil316 <indranil.karmakar@aot.edu.in>
2024-07-16 12:11:08 +0530

    added cors

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import bodyParser from 'body-parser';
 import * as configs from './configs/env.config.js';
 import mongoose from 'mongoose';
 import vendorAuthRoutes from './routes/vendorAuth.js';
+import upload from './configs/multer.config.js';
+import cors from 'cors';
 
 const PORT = configs.default.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
@@ -27,6 +30,12 @@ mongoose
   });
 
 app.use('/api/auth/vendor', vendorAuthRoutes);
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  // Handle uploaded file here
+  console.log('File uploaded:', req.file);
+  // Return a response to confirm upload success
+  res.json({ message: 'File uploaded successfully', file: req.file });
+});
 
 app.get('/', (req, res, next) => {
   res.send('hi');
